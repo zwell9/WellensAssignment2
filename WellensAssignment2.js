f8951ba3704f3ed033568b02b039ff2e8157c4a3
zwell9 <zach.wellens@drake.edu>
2021-02-15 23:40:58 -0600

    Fixed errors and finished program

--- WellensAssignment2.js
+++ WellensAssignment2.js
@@ -14,16 +14,50 @@ window.onload = function init()
     gl = canvas.getContext("webgl2");
 
     colors = [
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        
+        // 1st triangle
+        vec3(1.0, 0.0, 0.0),
+        vec3(1.0, 0.0, 0.0),
+        vec3(1.0, 0.0, 0.0),
+        //2nd triangle
+        vec3(0.0, 0.0, 1.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(0.0, 0.0, 1.0),
+        //3rd triangle
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        //4th triangle
+        vec3(0.5, 0.2, 0.3),
+        vec3(0.2, 0.1, 0.7),
+        vec3(0.4, 0.4, 0.2),
+        //5th triangle
+        vec3(0.4, 0.3, 0.3),
+        vec3(0.2, 0.2, 0.6),
+        vec3(0.1, 0.4, 0.5),
     ];
 
     points = [
-        -1, -1,
-        0, 1,
-        1, -1
+        // 1st triangle
+        vec2(-1, 1),
+        vec2(0.5, 1),
+        vec2(-0.5, 0),
+        // 2nd triangle
+        vec2(1, 1),
+        vec2(-0.5, 1),
+        vec2(0.5, 0),
+        // 3rd triangle
+        vec2(-0.5, 1),
+        vec2(0.5, 1),
+        vec2(0, 0),
+        // 4th triangle
+        vec2(-1, -1),
+        vec2(-1, 0.25),
+        vec2(0.5, -0.5),
+        // 5th triangle
+        vec2(1, -1),
+        vec2(1, 0.25),
+        vec2(-0.5, -0.5),
+        
     ];
 
     gl.viewport(0, 0, canvas.width, canvas.height);
@@ -44,12 +78,12 @@ window.onload = function init()
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    var aPosition = gl.getAttribLocation(program, "aPosiion");
+    var aPosition = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.drawArrays(gl.TRIANGLES, 0, 15);
 
 }
 
