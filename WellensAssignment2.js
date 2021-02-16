60b1b15bed62c5752e804db399f1683022b7b06d
Timothy Urness <timothy.urness@drake.edu>
2021-02-15 22:30:31 -0600

    a few bug fixes

--- WellensAssignment2.js
+++ WellensAssignment2.js
@@ -14,20 +14,33 @@ window.onload = function init()
     gl = canvas.getContext("webgl2");
 
     colors = [
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        
+        vec3(1.0, 0.0, 0.0), 
+        vec3(1.0, 0.0, 1.0),
+        vec3(0.0, 0.0, 1.0)
     ];
 
+    // And, add our vertices point into our array of points
     points = [
-        -1, -1,
-        0, 1,
-        1, -1
-    ];
+        vec2(-1, -1 ), //1st triangle
+        vec2( 0, 1 ), 
+        vec2(1,  -1) 
+        ];
+
+    // colors = [
+    //     1.0, 0.0, 0.0,
+    //     1.0, 0.0, 0.0,
+    //     1.0, 0.0, 0.0,
+        
+    // ];
+
+    // points = [
+    //     -1, -1,
+    //     0, 1,
+    //     1, -1
+    // ];
 
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(.9, .9, .9, 1.0); //make background light grey
 
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
@@ -44,12 +57,12 @@ window.onload = function init()
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    var aPosition = gl.getAttribLocation(program, "aPosiion");
+    var aPosition = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
 
 }
 
